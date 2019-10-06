66a91ef43d019bee211476f1880d955a311c37fc
James Hosken <jameswfhosken@gmail.com>
2019-10-06 12:31:02 -0400

    Add classes for player information
    Including a PlayerHandler class to deal with all players connected to server

--- application/public/sketch.js
+++ application/public/sketch.js
@@ -6,15 +6,23 @@ socket.on('connect', function () {
   console.log("Connected");
 });
 
+
+let State =  {STARTSCREEN:0, GAME:1, ENDCREEN:2}
+
+let player;
+
 function setup() {
 
+  let rando = Math.floor(Math.random() * 100);
+  player = new Player("TestName" + rando.toString());
+
+  socket.emit('playerinfo', {name: player.name})
   // Listen for tilt data
   socket.on('begin', function (data) {
-    tilt = data;
   });
 
   // Listen for shake data
-  socket.on('timeUpdate', function (data) {
+  socket.on('score', function (data) {
     shake = data;
   });
 
@@ -22,12 +30,23 @@ function setup() {
 
 }
 
-
-
-
-
-
 function draw() {
   background(20);
-  
 }
+
+
+class Player{
+  constructor(name){
+    this.name = name;
+    this.score = 0;
+  }
+
+  getName(){
+    return this.name;
+  }
+  getScore(){
+    return this.score;
+  }
+}
+
+
--- application/server.js
+++ application/server.js
@@ -4,10 +4,23 @@ let express = require('express');
 let app = express();
 let server = require('http').createServer(app).listen(port, function () {
   console.log('Server listening at port: ', port);
+  Activate();
 });
 
 // Tell server where to look for files
 app.use(express.static('public'));
+
+//Global Vars:
+let playerHandler;
+
+
+let Activate = function(){
+  //Activate runs once server is setup
+
+  playerHandler = new PlayerHandler();
+
+}
+
 
 // Create socket connection
 let io = require('socket.io').listen(server);
@@ -16,9 +29,84 @@ let io = require('socket.io').listen(server);
 io.on('connection', function (socket) {
   console.log('A client connected: ' + socket.id);
 
+  socket.on('setNumPlayers', function (data) {
+    playerHandler.updateRequiredPlayers(data);
+  });
+
+  // Add new player to list of players.
+  socket.on('playerinfo', function (data) {
+    let player = new Player(socket.id, data.name);
+    playerHandler.addPlayer(player);
+  });
+
   // Listen for this client to disconnect
   socket.on('disconnect', function () {
+    //If id is associated with player, remove player from list.
+    playerHandler.removePlayerByID(socket.id);
     console.log("A client has disconnected " + socket.id);
   });
 });
 
+
+
+class PlayerHandler{
+  //Loop through all connections and create player objects
+  constructor(){
+    this.players = [];
+    this.requiredPlayers = 10;
+  }
+  updateRequiredPlayers(num){
+    this.requiredPlayers = num;
+  }
+  addPlayer(player){
+    console.log("New player: " + player.name);
+    this.players.push(player);
+    this.printPlayers();
+  }
+
+  removePlayerByID(id){
+    let indexToRemove = -1;
+    for (let i = 0; i < this.players.length; i++) {
+      let player = this.players[i];
+      if(player.id == id){
+        indexToRemove = i;
+      }
+    }
+    if(indexToRemove > -1){
+      console.log("Player left: " + this.players[indexToRemove].name);
+      this.players.splice(indexToRemove, 1);
+      this.printPlayers();
+    }
+    
+  }
+
+  printPlayers(){
+    console.log("PLAYER LIST:")
+    for (let i = 0; i < this.players.length; i++) {
+      let player = this.players[i];
+      console.log("Player: " + i + ": " + player.name);
+
+      
+    }
+  }
+}
+
+class Player{
+  constructor(id, name){
+    this.id = id;
+    this.name = name;
+    this.score = 0;
+  }
+
+  updateScore(score){
+    this.score - score;
+  }
+
+  getScore(){
+    return this.score;
+  }
+
+  getName(){
+    return this.name;
+  }
+}
